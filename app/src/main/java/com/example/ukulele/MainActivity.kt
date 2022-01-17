fd213dd4d65713206fb0e9d16e13941937175700
mtnmr <mei.rila1013@gmail.com>
2022-01-17 17:52:32 +0900

    midiFile,touchEvent add

--- app/src/main/java/com/example/ukulele/MainActivity.kt
+++ app/src/main/java/com/example/ukulele/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.ukulele
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Point
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.Display
+import android.view.MotionEvent
 import android.view.SurfaceHolder
+import android.view.WindowManager
 import com.example.ukulele.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), SurfaceHolder.Callback{
@@ -13,6 +20,23 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback{
     private var frets: Array<Float> = arrayOf(0f, 0f, 0f, 0f)
     private var strings:Array<Float> = arrayOf(0f, 0f, 0f, 0f)
 
+    private var screenHeight: Int = 0
+    private var diffY: Int = 0
+
+    private var c5:MediaPlayer = MediaPlayer()
+    private var cs5:MediaPlayer = MediaPlayer()
+    private var d5:MediaPlayer = MediaPlayer()
+    private var ds5:MediaPlayer = MediaPlayer()
+    private var e5:MediaPlayer = MediaPlayer()
+    private var f5:MediaPlayer = MediaPlayer()
+    private var fs5:MediaPlayer = MediaPlayer()
+    private var g5:MediaPlayer = MediaPlayer()
+    private var gs5:MediaPlayer = MediaPlayer()
+    private var a5:MediaPlayer = MediaPlayer()
+    private var as5:MediaPlayer = MediaPlayer()
+    private var b5:MediaPlayer = MediaPlayer()
+    private var c6:MediaPlayer = MediaPlayer()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,13 +44,36 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback{
 
         val holder = binding.surfaceView.holder
         holder.addCallback(this)
+
+        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display: Display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        screenHeight = size.y
+
+        c5 = MediaPlayer.create(this, R.raw.guitar_c5)
+        cs5 = MediaPlayer.create(this, R.raw.guitar_cs5)
+        d5 = MediaPlayer.create(this, R.raw.guitar_d5)
+        ds5 = MediaPlayer.create(this, R.raw.guitar_ds5)
+        e5 = MediaPlayer.create(this, R.raw.guitar_e5)
+        f5 = MediaPlayer.create(this, R.raw.guitar_f5)
+        fs5 = MediaPlayer.create(this, R.raw.guitar_fs5)
+        g5 = MediaPlayer.create(this, R.raw.guitar_g5)
+        gs5 = MediaPlayer.create(this, R.raw.guitar_gs5)
+        a5 = MediaPlayer.create(this, R.raw.guitar_a5)
+        as5 = MediaPlayer.create(this, R.raw.guitar_as5)
+        b5 = MediaPlayer.create(this, R.raw.guitar_b5)
+        c6 = MediaPlayer.create(this, R.raw.guitar_c6)
+
     }
 
     override fun surfaceCreated(p0: SurfaceHolder) {
-        TODO("Not yet implemented")
+
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        diffY = screenHeight - height
+
         val canvas = binding.surfaceView.holder.lockCanvas()
         canvas.drawColor(Color.rgb(115,66,41))
 
@@ -55,6 +102,133 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback{
     }
 
     override fun surfaceDestroyed(p0: SurfaceHolder) {
-        TODO("Not yet implemented")
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var stringNo:Int = 0
+        var fretNo:Int = 0
+
+        if (event?.action == MotionEvent.ACTION_DOWN){
+            when (event.x){
+                in (strings[0]-200)..(strings[0]+200) -> stringNo = 4
+                in (strings[1]-200)..(strings[1]+200) -> stringNo = 3
+                in (strings[2]-200)..(strings[2]+200) -> stringNo = 2
+                in (strings[3]-200)..(strings[3]+200) -> stringNo = 1
+            }
+            when (event.y - diffY){
+                in (frets[0]+15f)..(frets[0]-15f) ->fretNo = 1
+                in (frets[1]+15f)..(frets[1]-15f) ->fretNo = 2
+                in (frets[2]+15f)..(frets[2]-15f) ->fretNo = 3
+            }
+
+            playTone(stringNo, fretNo)
+        }
+
+        return true
+    }
+
+    private fun playTone(stringNo: Int, fretNo: Int) {
+        when (stringNo){
+            4 ->{
+                when (fretNo){
+                    0 ->{
+                        g5.seekTo(0)
+                        g5.start()
+                    }
+                    1 ->{
+                        gs5.seekTo(0)
+                        gs5.start()
+                    }
+                    2 ->{
+                        a5.seekTo(0)
+                        a5.start()
+                    }
+                    3 ->{
+                        as5.seekTo(0)
+                        as5.start()
+                    }
+                }
+            }
+            3 ->{
+                when (fretNo){
+                    0 ->{
+                        c5.seekTo(0)
+                        c5.start()
+                    }
+                    1 ->{
+                        cs5.seekTo(0)
+                        cs5.start()
+                    }
+                    2 ->{
+                        d5.seekTo(0)
+                        d5.start()
+                    }
+                    3 ->{
+                        ds5.seekTo(0)
+                        ds5.start()
+                    }
+                }
+            }
+            2 ->{
+                when (fretNo){
+                    0 ->{
+                        e5.seekTo(0)
+                        e5.start()
+                    }
+                    1 ->{
+                        f5.seekTo(0)
+                        f5.start()
+                    }
+                    2 ->{
+                        fs5.seekTo(0)
+                        fs5.start()
+                    }
+                    3 ->{
+                        g5.seekTo(0)
+                        g5.start()
+                    }
+                }
+            }
+            1 ->{
+                when (fretNo){
+                    0 ->{
+                        a5.seekTo(0)
+                        a5.start()
+                    }
+                    1 ->{
+                        as5.seekTo(0)
+                        as5.start()
+                    }
+                    2 ->{
+                        b5.seekTo(0)
+                        b5.start()
+                    }
+                    3 ->{
+                        c6.seekTo(0)
+                        c6.start()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        c5.release()
+        cs5.release()
+        d5.release()
+        ds5.release()
+        e5.release()
+        f5.release()
+        fs5.release()
+        g5.release()
+        gs5.release()
+        a5.release()
+        as5.release()
+        b5.release()
+        c6.release()
+
+
     }
 }
